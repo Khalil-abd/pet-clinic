b3f89244d82a8a6f6af1416bd8598cfcff93a6b0
Khalil-abd <khalilabdellaoui@gmail.com>
2023-01-14 21:28:33 +0100

    Create Vet Index Page and Controller. closes #4.

---
+++ pet-clinic-web/src/main/java/com/ka/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.ka.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+
+        return "vets/index";
+    }
+}
